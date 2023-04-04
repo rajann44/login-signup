6c4317534c1f16bdb092d66127f1d3d50ffa3480
Rajan Chaudhary <rajann44@gmail.com>
2023-04-04 14:09:41 +0200

    useLocation Hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
