e5d43c5cc417078b415113ec1efe5dd820ed58b6
Rajan Chaudhary <rajann44@gmail.com>
2023-04-04 18:46:20 +0200

    style modules

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import homestyle from "../style/home.module.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div class={"container " + homestyle.container}>
+        <h1 class={homestyle.h1}>Welcome to my Login/Signup Web</h1>
+        <p class={homestyle.p}>
+          This is a simple homepage with a navbar and footer.
+        </p>
+        <p>Homepage CSS is being rendered using style modules</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
